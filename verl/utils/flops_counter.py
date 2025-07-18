09aecd7a8f645385b341a86294db1e1cf62c4a44
rbao2018 <rbao2018@outlook.com>
2025-07-18 16:44:54 +0800

    supprt bailing_moe flops counter

--- verl/utils/flops_counter.py
+++ verl/utils/flops_counter.py
@@ -16,7 +16,7 @@ from transformers import PretrainedConfig
 
 from verl.utils.device import get_torch_device
 
-VALID_CONFIG_TYPE = {"llama", "qwen2", "qwen2_vl", "qwen2_5_vl", "qwen3", "qwen3_moe", "deepseek_v3", "minicpmv", "minicpmo"}
+VALID_CONFIG_TYPE = {"llama", "qwen2", "qwen2_vl", "qwen2_5_vl", "qwen3", "qwen3_moe", "deepseek_v3", "minicpmv", "minicpmo", "bailing_moe"}
 
 
 def get_device_flops(unit="T"):
@@ -41,8 +41,8 @@ def get_device_flops(unit="T"):
         flops = 312e12
     elif "L40" in device_name:
         flops = 181.05e12
-    elif "L20" in device_name:
-        flops = 119.5e12
+    elif "L20Y" in device_name:
+        flops = 989e12
     elif "H20" in device_name:
         flops = 148e12
     elif "910B" in device_name:
@@ -71,6 +71,7 @@ class FlopsCounter:
             "qwen2": self._estimate_qwen2_flops,
             "llama": self._estimate_qwen2_flops,
             "qwen2_moe": self._estimate_qwen2_moe_flops,
+            "bailing_moe": self._estimate_qwen2_moe_flops,
             "qwen2_vl": self._estimate_qwen2_flops,
             "qwen2_5_vl": self._estimate_qwen2_flops,
             "qwen3": self._estimate_qwen2_flops,
